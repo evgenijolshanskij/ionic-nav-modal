8d19f23bdf575393321b67dd946071756c767789
Evgenij Olshanskij <gekanchiko@gmail.com>
2016-01-04 11:17:59 +0200

    Init commit

---
+++ www/js/controllers.js
@@ -0,0 +1,94 @@
+angular.module('starter.controllers', [])
+
+.controller('AppCtrl', function($scope, $ionicModal, $timeout) {
+
+  // With the new view caching in Ionic, Controllers are only called
+  // when they are recreated or on app start, instead of every page change.
+  // To listen for when this page is active (for example, to refresh data),
+  // listen for the $ionicView.enter event:
+  //$scope.$on('$ionicView.enter', function(e) {
+  //});
+
+  // Form data for the login modal
+  $scope.loginData = {};
+
+  // Create the login modal that we will use later
+  $ionicModal.fromTemplateUrl('templates/login.html', {
+    scope: $scope
+  }).then(function(modal) {
+    $scope.modal = modal;
+  });
+
+  // Triggered in the login modal to close it
+  $scope.closeLogin = function() {
+    $scope.modal.hide();
+  };
+
+  // Open the login modal
+  $scope.login = function() {
+    $scope.modal.show();
+  };
+
+  // Perform the login action when the user submits the login form
+  $scope.doLogin = function() {
+    console.log('Doing login', $scope.loginData);
+
+    // Simulate a login delay. Remove this and replace with your login
+    // code if using a login system
+    $timeout(function() {
+      $scope.closeLogin();
+    }, 1000);
+  };
+
+    $scope.myGoBack = function() {
+      //$ionicNavBarDelegate.back();
+      $window.history.back();
+    };
+
+    $scope.hideAbout = true;
+
+    $scope.menu = [
+      {name: 'home', url: '../templates/about-home.html', isActive: true},
+      {name: 'section1', url: '../templates/about-section-1.html', isActive: false},
+      {name: 'section2', url: '../templates/about-section-2.html', isActive: false}
+    ];
+
+    function setActive(name) {
+      angular.forEach($scope.menu, function (item) {
+        item.isActive = (item.name === name);
+      });
+    }
+
+    $scope.showMenu = function (name) {
+      setActive(name);
+    };
+
+    $scope.viewAbout = function(){
+      $scope.hideAbout = false;
+    };
+
+    $scope.closeAbout = function () {
+      if (!$scope.hideAbout) {
+        $scope.hideAbout = true;
+      } else {
+        $scope.myGoBack();
+      }
+      setActive('home');
+    };
+
+  })
+
+.controller('PlaylistsCtrl', function($scope) {
+  $scope.playlists = [
+    { title: 'Reggae', id: 1 },
+    { title: 'Chill', id: 2 },
+    { title: 'Dubstep', id: 3 },
+    { title: 'Indie', id: 4 },
+    { title: 'Rap', id: 5 },
+    { title: 'Cowbell', id: 6 }
+  ];
+})
+
+.controller('PlaylistCtrl', function($scope) {
+});
+
